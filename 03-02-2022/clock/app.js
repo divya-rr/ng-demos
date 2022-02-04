44e86690fd08bb9d1aac4c3060e32a95cb612c9d
divya-rr <divya.r@codestoresolutions.com>
2022-02-04 18:55:36 +0530

    clock

--- 03-02-2022/clock/app.js
+++ 03-02-2022/clock/app.js
@@ -5,12 +5,18 @@ alarmTime=null;
 alarmTimeout=null
 function updateTime(){
     const date = new Date();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const hours = date.getHours()
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
+    const hours = formatTime(date.getHours());
     display.innerText = `${hours}:${minutes}:${seconds}`
     
 
+}
+function formatTime(time){
+    if(time<10){
+        return '0' + time;
+    }
+    return time;
 }
 setInterval(updateTime,1000)
 function setAlarmTime(value){
