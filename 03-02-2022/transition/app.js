b53f17e43d2461545043e6903c7493eb8ffad17a
divya-rr <divya.r@codestoresolutions.com>
2022-02-04 18:52:50 +0530

    transition

--- 03-02-2022/transition/app.js
+++ 03-02-2022/transition/app.js
@@ -1,11 +1,12 @@
 function changeBG(ele,color){
     var element =document.getElementById(ele);
-element.style.transition = "background 1.0s linear 1s";
+element.style.transition = "background 1.0s linear 0s";
 element.style.background = color;
 
 }
 function fadeIn(elem){
-    var element=document.getElementById(elem)
+    var element=document.getElementById(elem);
+    element.style.height="100px";
     element.style.transition = "opacity 0.1s linear 0s";
     element.style.opacity =1;
 
@@ -18,25 +19,33 @@ function fadeOut(elem){
 }
 function slideIn(elem){
     var element=document.getElementById(elem)
-    element.style.transition = "left 0.1s ease-in 0s";
-    element.style.left="0px";
+    // element.style.left="-120px"
+    
+   
+    
+    element.style.transition = "width 1s ease-out 0s";
+     element.style.width="0px";
+     element.style.height="100px";
 
 }
 function slideOut(elem){
     var element=document.getElementById(elem)
-    element.style.transition = "left 1.0s ease-out 0s";
-    element.style.left="-600px";
+    element.style.transition = "left 1s ease-out 0s";
+    element.style.left="-120px";
 
 }
 function slideOpen(elem){
     var element=document.getElementById(elem)
+    element.style.height="0px"
     element.style.transition = "height 1.0s linear 0s";
-    element.style.height="40px";
+    element.style.height="100px";
 
 }
 function slideClosed(elem){
     var element=document.getElementById(elem)
     element.style.transition = "height 1.0s linear 0s";
+    
     element.style.height="0px";
+    
 
 }
