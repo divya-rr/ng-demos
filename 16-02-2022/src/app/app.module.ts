2a2e6a7c6308e033b15d6af704fb927829e76fbc
divya-rr <divya.r@codestoresolutions.com>
2022-02-16 19:07:34 +0530

    nested dropdown

---
+++ 16-02-2022/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import { ClassesComponent } from './classes/classes.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClassesComponent,
+   
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
